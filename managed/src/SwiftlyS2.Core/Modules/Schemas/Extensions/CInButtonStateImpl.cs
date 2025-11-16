ff21db84c05f1e1d655cca79446b927c612a7112
samyyc <s1myyc@outlook.com>
2025-11-16 19:36:52 +0800

    fix(managed): fix build

--- managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CInButtonStateImpl.cs
+++ managed/src/SwiftlyS2.Core/Modules/Schemas/Extensions/CInButtonStateImpl.cs
@@ -5,10 +5,22 @@ namespace SwiftlyS2.Core.SchemaDefinitions;
 
 internal partial class CInButtonStateImpl : CInButtonState
 {
-  public GameButtonFlags ButtonPressed => (GameButtonFlags)ButtonStates[0];
+  public GameButtonFlags ButtonPressed
+  {
+    get => (GameButtonFlags)ButtonStates[0];
+    set => ButtonStates[0] = (ulong)value;
+  }
 
-  public GameButtonFlags ButtonChanged => (GameButtonFlags)ButtonStates[1];
+  public GameButtonFlags ButtonChanged
+  {
+    get => (GameButtonFlags)ButtonStates[1];
+    set => ButtonStates[1] = (ulong)value;
+  }
 
-  public GameButtonFlags ButtonScroll => (GameButtonFlags)ButtonStates[2];
-  
+  public GameButtonFlags ButtonScroll
+  {
+    get => (GameButtonFlags)ButtonStates[2];
+    set => ButtonStates[2] = (ulong)value;
+  }
+
 }
